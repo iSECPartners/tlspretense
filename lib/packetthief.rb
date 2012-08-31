aacb996baf7031a924a5ae67f3ea2143fe8ad0e7
William (B.J.) Snow Orvis <aetherknight@gmail.com>
2012-08-31 15:55:15 -0700

    Making the PacketThief module delegate to an implementation, if known.
    For example, Linux supports Netfilter, and Mac OS X 10.6 supports Ipfw.

--- lib/packetthief.rb
+++ lib/packetthief.rb
@@ -2,4 +2,18 @@ module PacketThief
   autoload :RedirectRule, 'packetthief/redirect_rule'
   autoload :Netfilter, 'packetthief/netfilter'
   autoload :Ipfw, 'packetthief/ipfw'
+
+  def self.method_missing(m, *args, &block)
+    case RUBY_PLATFORM
+    when /linux/
+      Netfilter.send(m, *args, &block)
+    when /darwin10/ # Mac OS X 10.6 and earlier.
+      Ipfw.send(m, *args, &block)
+    else
+      raise "Platform #{RUBY_PLATFORM} not yet supported! If you know your network implementation, call it directly."
+    end
+  end
+
+
+
 end
