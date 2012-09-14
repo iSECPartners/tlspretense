fbc40aa49c6ec8aad9f8d09f5e52645b1b51ee0a
William (B.J.) Snow Orvis <aetherknight@gmail.com>
2012-09-13 22:50:14 -0700

    Adding a ProxyRedirector.
    It demonstrates how you could redirect connections to a different
    destination.

--- lib/packetthief/emhandlers.rb
+++ lib/packetthief/emhandlers.rb
@@ -3,6 +3,7 @@ require 'eventmachine'
 module PacketThief
   module EMHandlers
     autoload :TransparentProxy, 'packetthief/emhandlers/transparent_proxy'
+    autoload :ProxyRedirector,  'packetthief/emhandlers/proxy_redirector'
   end
 
   # Alias for EMHandlers
---
+++ lib/packetthief/emhandlers/proxy_redirector.rb
@@ -0,0 +1,26 @@
+module PacketThief
+  module EMHandlers
+
+    # Instead of forwarding the connection to the original host, forwards it to
+    # a configured host instead.
+    class ProxyRedirector < TransparentProxy
+
+      def initialize(proxy_host, proxy_port)
+        super()
+
+        @proxy_host = proxy_host
+        @proxy_port = proxy_port
+      end
+
+      # Instead of using the original destination, use the configured destination.
+      def client_connected
+        dest_host = @proxy_host
+        dest_port = @proxy_port
+        connect_to_dest
+      end
+
+    end
+
+  end
+end
+
