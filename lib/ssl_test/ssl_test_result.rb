51b89381b3c5e3a4bedad455ee1432b7fb5d1ab8
William (B.J.) Snow Orvis <bjorvis@isecpartners.com>
2012-10-09 16:31:20 -0700

    Added attributes to SSLTestResult.

--- lib/ssl_test/ssl_test_result.rb
+++ lib/ssl_test/ssl_test_result.rb
@@ -1,6 +1,14 @@
 module SSLTest
   class SSLTestResult
-    def initialize(testcase_id, pass=true)
+
+    attr_reader   :id
+    attr_accessor :description
+    attr_accessor :expected_result, :actual_result
+    attr_accessor :start_time, :stop_time
+
+    def initialize(testcase_id, passed=true)
+      @id = testcase_id
+      @passed = true
     end
   end
 end
