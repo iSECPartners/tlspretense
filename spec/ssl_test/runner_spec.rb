20f1eaf2c625a1c2c1825e81af92ee0a2b24452a
William (B.J.) Snow Orvis <bjorvis@isecpartners.com>
2012-10-09 14:21:50 -0700

    Updating the runner spec to use subject

--- spec/ssl_test/runner_spec.rb
+++ spec/ssl_test/runner_spec.rb
@@ -2,8 +2,10 @@ require File.expand_path(File.join(File.dirname(__FILE__),'..','spec_helper'))
 
 module SSLTest
   describe Runner do
+    let(:args) { [] }
     let(:stdin) { double("stdin") }
     let(:stdout) { double("stdout") }
+
     let(:test_foo) { double('test foo') }
     let(:test_bar) { double('test bar') }
     let(:test_data) { [test_foo, test_bar] }
@@ -15,6 +17,7 @@ module SSLTest
     let(:testcaseresult) { double('test case result') }
     let(:testcase) { double('test case', :run => testcaseresult) }
 
+    subject { Runner.new(args, stdin, stdout) }
 
     before(:each) do
       Config.stub(:load_conf).and_return(config)
@@ -28,19 +31,17 @@ module SSLTest
       context "when ARGS is empty" do
         let(:args) { [] }
         it "loads the config from the default location" do
-          @runner = Runner.new(args, stdin, stdout)
           Config.should_receive(:new).and_return(config)
-          @runner.stub(:run_test)
+          subject.stub(:run_test)
 
-          @runner.run
+          subject.run
         end
 
         it "initializes a certificate manager" do
           CertificateManager.should_receive(:new).with(conf_certs).and_return(cert_manager)
 
-          @runner = Runner.new(args, stdin, stdout)
-          @runner.stub(:run_test)
-          @runner.run
+          subject.stub(:run_test)
+          subject.run
         end
 
       end
@@ -51,11 +52,10 @@ module SSLTest
         let(:args) { [] }
 
         it "runs all defined tests" do
-          @runner = Runner.new(args, stdin, stdout)
-          @runner.should_receive(:run_test).with(test_foo).ordered
-          @runner.should_receive(:run_test).with(test_bar).ordered
+          subject.should_receive(:run_test).with(test_foo).ordered
+          subject.should_receive(:run_test).with(test_bar).ordered
 
-          @runner.run
+          subject.run
         end
       end
 
@@ -64,10 +64,9 @@ module SSLTest
 
         it "it runs just the test named 'wrongcname'" do
           config.stub(:tests).with(['wrongcname']).and_return([test_wrongcname])
-          @runner = Runner.new(args, stdin, stdout)
-          @runner.should_receive(:run_test).with(test_wrongcname)
+          subject.should_receive(:run_test).with(test_wrongcname)
 
-          @runner.run
+          subject.run
         end
 
       end
@@ -79,21 +78,18 @@ module SSLTest
       it "creates an ssl test case" do
         SSLTestCase.should_receive(:new).with(config, cert_manager, report, test_data)
 
-        @runner = Runner.new([],stdin, stdout)
-        @runner.run_test test_data
+        subject.run_test test_data
       end
       it "then runs the test case" do
         testcase.should_receive(:run).and_return(testcaseresult)
 
-        @runner = Runner.new([],stdin, stdout)
-        @runner.run_test test_data
+        subject.run_test test_data
       end
       it "stores the result" do
-        @runner = Runner.new([],stdin, stdout)
 
-        @runner.results.should_receive(:<<).with(testcaseresult)
+        subject.results.should_receive(:<<).with(testcaseresult)
 
-        @runner.run_test test_data
+        subject.run_test test_data
       end
 
     end
