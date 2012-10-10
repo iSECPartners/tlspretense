260af979f5f764e6d0cf3b59104838ec42670ca0
William (B.J.) Snow Orvis <bjorvis@isecpartners.com>
2012-10-10 15:08:51 -0700

    Adding a wrapper to easily call `sudo bundle exec...`

---
+++ bin/ssl_test_runner_wrapper.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+rbversion=$(ruby --version)
+
+if [ "${RUBY}x" == "x" ] ; then
+    if ruby --version | grep -q '1\.9' ; then
+        RUBY=ruby
+    elif ruby1.9 --version | grep -q '1\.9' ; then
+        RUBY=ruby1.9
+    fi
+fi
+
+sudo bundle exec ${RUBY} bin/ssl_test_runner.rb
